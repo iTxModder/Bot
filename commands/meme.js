793d2d2e5cce5602209bb6b6ad8cab6cd5b8de60
iTxModder <59070565+iTxModder@users.noreply.github.com>
2019-12-23 13:23:52 -0300

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports.run = async (bot, message, args) => {
+  if(message.author.bot) return;
+  if(message.channel.type !=="text") return;
+  
+   randomPuppy('memes')
+  .then(url => {
+                const embed = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setImage(url)
+                    .setColor(`${message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : 0xffffff}`)
+                     message.channel.send({ embed });
+  })
+}
+
+exports.command = {
+    name: 'meme',
+    permission: "Nenhuma",
+    description: 'Vê um meme!',
+    category: "fun",
+    usage: ['*meme'],
+    enabled: true
+}
